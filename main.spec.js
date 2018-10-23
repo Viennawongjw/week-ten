005517863d28db54828d63f5f29a8f2264649d35
Yen Ter <leeyenter@users.noreply.github.com>
2018-10-23 15:29:48 +0800

    Add tests

--- main.spec.js
+++ main.spec.js
@@ -7,7 +7,11 @@ describe("Vue component tests", () => {
   });
   it("The component should have a title property.", () => {
     const defaultData = App.data();
-    // expect(defaultData.title).toBe("BT3103 Week 10");
+    expect(defaultData.title);
+  });
+  it("The component's title should be BT3103 Week 10", () => {
+    const defaultData = App.data();
+    expect(defaultData.title).toBe("BT3103 Week 10");
   });
 
   it("The component should have an updateCurrentChart function.", () => {
